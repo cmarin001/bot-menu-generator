e9b747e74d78367d69e988b88280c03a45987f5e
cmarin001 <cmarin6051@gmail.com>
2025-06-10 11:46:32 -0500

    fix: added logs to generating images

--- utils/renderImage.ts
+++ utils/renderImage.ts
@@ -4,12 +4,13 @@ import puppeteer from "puppeteer";
 async function renderHTMLToImage(html: string, imagePath: string): Promise<void> {
   console.log("🧪 Launching browser...");
   const browser = await puppeteer.launch({
+    executablePath: puppeteer.executablePath(),
     headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
   });
 
   const page = await browser.newPage();
-    console.log("🧪 Setting page content...");
+  console.log("🧪 Setting page content...");
   await page.setContent(html, { waitUntil: "networkidle0" });
   console.log("🧪 Taking screenshot...");
   await page.screenshot({ path: imagePath });
