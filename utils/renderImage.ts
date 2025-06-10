48dc07cb48bfaba31a92e4800b44068768166cb5
cmarin001 <cmarin6051@gmail.com>
2025-06-10 17:05:03 -0500

    fix: update config for generating image

--- utils/renderImage.ts
+++ utils/renderImage.ts
@@ -1,24 +1,19 @@
-import puppeteer from "puppeteer-core";
-import chromium from "chrome-aws-lambda";
+import puppeteer from "puppeteer";
 import fs from "fs";
 
 export async function renderHTMLToImage(html: string, imagePath: string): Promise<void> {
-  console.log("🧪 Launching browser with chrome-aws-lambda...");
+  console.log("🧪 Launching browser with puppeteer...");
 
   const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: (await chromium.executablePath) || "/usr/bin/google-chrome-stable",
-    headless: chromium.headless,
-    defaultViewport: chromium.defaultViewport
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: true,
   });
 
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: "networkidle0" });
   const buffer = await page.screenshot();
-
-  console.log("📸 Screenshot captured, saving...");
-  fs.writeFileSync(imagePath, buffer!);
-  console.log("✅ Screenshot saved:", imagePath);
-
+  fs.writeFileSync(imagePath, buffer);
   await browser.close();
+
+  console.log("✅ Screenshot saved:", imagePath);
 }
